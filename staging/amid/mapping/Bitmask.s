a3bbeb017a90251632646ac678878135728fe44f
volodymyrmigdal <volodymyr.migdal@gmail.com>
2016-10-10 14:50:18 +0300

    mapToWord docs update

--- staging/amid/mapping/Bitmask.s
+++ staging/amid/mapping/Bitmask.s
@@ -61,10 +61,9 @@ var init = function( o )
 //
 
 /**
- * Converts boolean map( map ) into  32-bit number represetation.
+ * Converts boolean map( map ) into  32-bit number bitmask.
  * Each true/false key value in map corresponds to 1/0 bit value in number.
- * Before converions function supplements source( map ) by default fields
- * from( defaultFieldsMap ) that map doesn`t contain.
+ * Before converions function supplements source( map ) by unique fields from( defaultFieldsMap ).
  *
  * @param { object } map - source map.
  * @return { number } Returns boolean map values represented as number.
